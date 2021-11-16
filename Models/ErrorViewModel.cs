94ad4b31a64bc785663e8fbeffa7370febae850a
sreesyamgit <sreesyam@gmail.com>
2021-11-16 17:04:32 +0530

    First commit MVC application to Local repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace calc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
